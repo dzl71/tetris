8f646fb89e3ac462ea6ffadd79c692634c22dc9a
dzl71 <129626835+dzl71@users.noreply.github.com>
2023-05-18 18:58:45 +0300

    created the skeleton of the code
    most of the functions from the flowchart were added

---
+++ blocks.py
@@ -0,0 +1,16 @@
+class block:
+    
+    def __init__(self) -> None:
+        pass
+    
+    def rotate_right(self):
+        pass
+    
+    def rotate_left(self):
+        pass
+    
+    def move_down(self):
+        pass
+    
+    def fall(self):
+        pass
---
+++ main.py
@@ -0,0 +1,25 @@
+import blocks as blk
+
+def create_board() -> list[list]:
+    return
+
+def print_board(board: list[list]):
+    pass
+
+def collect_blocks() -> list[blk.blocks]:
+    return
+
+def insert_block_to_board(board: list[list], block: blk.block):
+    pass
+
+def is_row_full(board: list) -> bool:
+    return
+
+def delete_row():
+    pass
+
+def is_board_full(board: list[list]) -> bool:
+    return
+
+def main():
+    pass
