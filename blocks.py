0636cabdd6b8f7f42add31ad34978bffcd6fb72e
dzl71 <129626835+dzl71@users.noreply.github.com>
2023-05-25 23:48:11 +0300

    created blocks

--- blocks.py
+++ blocks.py
@@ -1,10 +1,55 @@
-class block:
-    def __init__(self, ) -> None:
-        self.falling = True
-        
+import random as rnd
+
+BLOCK_CHAR = '[]'
+BLANK = "  "
+
+shapes = {
+    'I' : [[BLOCK_CHAR, BLANK, BLANK],
+            [BLOCK_CHAR, BLANK, BLANK],
+            [BLOCK_CHAR, BLANK, BLANK],
+            [BLOCK_CHAR, BLANK, BLANK]],
     
+    'T' : [[BLOCK_CHAR, BLOCK_CHAR, BLOCK_CHAR],
+            [BLOCK_CHAR, BLOCK_CHAR, BLANK],
+            [BLANK, BLANK, BLANK],
+            [BLANK, BLANK, BLANK]],
+    
+    'Z' : [[BLOCK_CHAR, BLOCK_CHAR, BLANK],
+            [BLANK, BLOCK_CHAR, BLOCK_CHAR],
+            [BLANK, BLANK, BLANK],
+            [BLANK, BLANK, BLANK]],
+    
+    'S' : [[BLANK, BLOCK_CHAR, BLOCK_CHAR],
+            [BLOCK_CHAR, BLOCK_CHAR, BLANK],
+            [BLANK, BLANK, BLANK],
+            [BLANK, BLANK, BLANK]],
+    
+    'L' : [[BLOCK_CHAR, BLANK, BLANK],
+            [BLOCK_CHAR, BLANK, BLANK],
+            [BLOCK_CHAR, BLOCK_CHAR, BLANK],
+            [BLANK, BLANK, BLANK]],
+    
+    'J' : [[BLANK, BLOCK_CHAR, BLANK],
+            [BLANK, BLOCK_CHAR, BLANK],
+            [BLOCK_CHAR, BLOCK_CHAR, BLANK],
+            [BLANK, BLANK, BLANK]],
+    
+    'O' : [[BLOCK_CHAR, BLOCK_CHAR, BLANK],
+            [BLOCK_CHAR, BLOCK_CHAR, BLANK],
+            [BLANK, BLANK, BLANK],
+            [BLANK, BLANK, BLANK]]
+}
+
+class block:
+    def __init__(self) -> BLANK:
+        self.falling = True
+        self.name = rnd.choice(list(shapes))
+        self.shape = shapes[self.name]
+        
     def rotate_right(self):
-        pass
+        new_shape = [[], [], [], []]
+        for idx in range(-1, -4, -1):
+        
     
     def rotate_left(self):
         pass
@@ -14,15 +59,3 @@ class block:
     
     def fall(self):
         pass
-
-block_char = '[]'
-
-shapes = [
-    f'''{{4 * (block_char + '\n')}}''',
-    f''' ''',
-    f''' ''',
-    f''' ''',
-    f''' ''',
-    f''' ''',
-    
-]
