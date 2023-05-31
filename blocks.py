ad9b39e83aed7560d401d5fbf8f318cafdffd13b
dzl71 <129626835+dzl71@users.noreply.github.com>
2023-05-31 17:12:58 +0300

    added the key_reader, create_board, print_board, is_row_full, delete_row, and board_full functions

--- blocks.py
+++ blocks.py
@@ -4,50 +4,91 @@ BLOCK_CHAR = '[]'
 BLANK = "  "
 
 shapes = {
-    'I' : [[BLOCK_CHAR, BLANK, BLANK],
-            [BLOCK_CHAR, BLANK, BLANK],
-            [BLOCK_CHAR, BLANK, BLANK],
-            [BLOCK_CHAR, BLANK, BLANK]],
+    'I' : 3 * (BLOCK_CHAR + '\n') + BLOCK_CHAR,
     
-    'T' : [[BLOCK_CHAR, BLOCK_CHAR, BLOCK_CHAR],
-            [BLOCK_CHAR, BLOCK_CHAR, BLANK],
-            [BLANK, BLANK, BLANK],
-            [BLANK, BLANK, BLANK]],
+    'T' : 3 * BLOCK_CHAR + '\n' + BLANK + BLOCK_CHAR,
     
-    'Z' : [[BLOCK_CHAR, BLOCK_CHAR, BLANK],
-            [BLANK, BLOCK_CHAR, BLOCK_CHAR],
-            [BLANK, BLANK, BLANK],
-            [BLANK, BLANK, BLANK]],
+    'Z' : 2 * BLOCK_CHAR + '\n' + BLANK + 2 * BLOCK_CHAR,
     
-    'S' : [[BLANK, BLOCK_CHAR, BLOCK_CHAR],
-            [BLOCK_CHAR, BLOCK_CHAR, BLANK],
-            [BLANK, BLANK, BLANK],
-            [BLANK, BLANK, BLANK]],
+    'S' : BLANK + 2 * BLOCK_CHAR + '\n' + 2 * BLOCK_CHAR,
     
-    'L' : [[BLOCK_CHAR, BLANK, BLANK],
-            [BLOCK_CHAR, BLANK, BLANK],
-            [BLOCK_CHAR, BLOCK_CHAR, BLANK],
-            [BLANK, BLANK, BLANK]],
+    'L' : 2 * (BLOCK_CHAR + '\n') + 2 * BLOCK_CHAR,
     
-    'J' : [[BLANK, BLOCK_CHAR, BLANK],
-            [BLANK, BLOCK_CHAR, BLANK],
-            [BLOCK_CHAR, BLOCK_CHAR, BLANK],
-            [BLANK, BLANK, BLANK]],
+    'J' : 2 * (BLANK + BLOCK_CHAR + '\n') + 2 * BLOCK_CHAR,
     
-    'O' : [[BLOCK_CHAR, BLOCK_CHAR, BLANK],
-            [BLOCK_CHAR, BLOCK_CHAR, BLANK],
-            [BLANK, BLANK, BLANK],
-            [BLANK, BLANK, BLANK]]
+    'O' : 2 * BLOCK_CHAR + '\n' + 2 * BLOCK_CHAR
+}
+
+shapes_r = {
+    'I' : 3 * (BLOCK_CHAR + '\n') + BLOCK_CHAR,
+    
+    'T' : 3 * BLOCK_CHAR + '\n' + BLANK + BLOCK_CHAR,
+    
+    'Z' : 2 * BLOCK_CHAR + '\n' + BLANK + 2 * BLOCK_CHAR,
+    
+    'S' : BLANK + 2 * BLOCK_CHAR + '\n' + 2 * BLOCK_CHAR,
+    
+    'L' : 2 * (BLOCK_CHAR + '\n') + 2 * BLOCK_CHAR,
+    
+    'J' : 2 * (BLANK + BLOCK_CHAR + '\n') + 2 * BLOCK_CHAR,
+    
+    'O' : 2 * BLOCK_CHAR + '\n' + 2 * BLOCK_CHAR
+}
+
+shapes_l = {
+    'I' : 3 * (BLOCK_CHAR + '\n') + BLOCK_CHAR,
+    
+    'T' : 3 * BLOCK_CHAR + '\n' + BLANK + BLOCK_CHAR,
+    
+    'Z' : 2 * BLOCK_CHAR + '\n' + BLANK + 2 * BLOCK_CHAR,
+    
+    'S' : BLANK + 2 * BLOCK_CHAR + '\n' + 2 * BLOCK_CHAR,
+    
+    'L' : 2 * (BLOCK_CHAR + '\n') + 2 * BLOCK_CHAR,
+    
+    'J' : 2 * (BLANK + BLOCK_CHAR + '\n') + 2 * BLOCK_CHAR,
+    
+    'O' : 2 * BLOCK_CHAR + '\n' + 2 * BLOCK_CHAR
+}
+
+shapes_180 =  {
+    'I' : 3 * (BLOCK_CHAR + '\n') + BLOCK_CHAR,
+    
+    'T' : 3 * BLOCK_CHAR + '\n' + BLANK + BLOCK_CHAR,
+    
+    'Z' : 2 * BLOCK_CHAR + '\n' + BLANK + 2 * BLOCK_CHAR,
+    
+    'S' : BLANK + 2 * BLOCK_CHAR + '\n' + 2 * BLOCK_CHAR,
+    
+    'L' : 2 * (BLOCK_CHAR + '\n') + 2 * BLOCK_CHAR,
+    
+    'J' : 2 * (BLANK + BLOCK_CHAR + '\n') + 2 * BLOCK_CHAR,
+    
+    'O' : 2 * BLOCK_CHAR + '\n' + 2 * BLOCK_CHAR
 }
 
 class block:
     def __init__(self) -> None:
         self.falling = True
+        self.rotated_right_amount = 0
+        self.rotated_left_amount = 0
         self.name = rnd.choice(list(shapes))
         self.shape = shapes[self.name]
         
     def rotate_right(self):
-        pass
+        match self.rotated_right_amount:
+            case 0:
+                self.shape = shapes_r[self.name]
+                self.rotated_right_amount = 1,
+            case 1: 
+                self.shape = shapes_180[self.name],
+                self.rotated_right_amount = 2
+            case 2:
+                self.shape = shapes_l[self.name],
+                self.rotated_right_amount = 3,
+            case 3:
+                self.shape = shapes[self.name],
+                self.rotated_right_amount = 0
     
     def rotate_left(self):
         pass
@@ -57,3 +98,12 @@ class block:
     
     def fall(self):
         pass
+    
+    def move_left(self):
+        pass
+
+    def move_right(self):
+        pass
+
+for item in shapes.values():
+        print(item + '\n')
--- main.py
+++ main.py
@@ -1,25 +1,73 @@
 import blocks as blk
+import time
+import keyboard as kb  # it resolves the problem of the input
+
+BOARD_WIDTH = 10
+BOARD_HEIGHT = 20
+STARTING_POSITION_PTR = 4
 
 def create_board() -> list[list]:
-    return
+    board = []
+    for _ in range(BOARD_HEIGHT):
+        row = []
+        for tile in range(BOARD_HEIGHT):
+            row.append(None)
+        board.append(row)
+    return board
 
 def print_board(board: list[list]):
-    pass
-
-def collect_blocks() -> list[blk.blocks]:
-    return
+    for row in board:
+        for tile in row:
+            if tile == None:
+                print("  ", end='')
+            else:
+                print(tile, end='')
+        print()
 
 def insert_block_to_board(board: list[list], block: blk.block):
     pass
 
-def is_row_full(board: list) -> bool:
-    return
+def is_row_full(board: list[list], row: int) -> bool:
+    for tile in board[row]:
+        if tile == None:
+            return False
+    return True
 
-def delete_row():
-    pass
+def delete_row(board: list[list], row_num: int):
+    for item in board[row_num]:
+        item = None
 
-def is_board_full(board: list[list]) -> bool:
-    return
+def board_full(board: list[list]) -> bool:
+    return False
 
 def main():
-    pass
+    board = create_board()
+    while not board_full(board):
+        block = blk.block()
+        insert_block_to_board(board, block)
+        
+        while block.falling:
+            
+            #insert the checker if able to fall and change the block state
+            
+            if kb.is_pressed('d'):
+                block.move_right()
+                
+            if kb.is_pressed('a'):
+                block.move_left()
+                
+            if kb.is_pressed('s'):
+                block.fall()
+               
+            if kb.is_pressed('k'):
+                block.rotate_left()
+               
+            if kb.is_pressed('l'):
+                block.rotate_right()
+                
+            time.sleep(1)
+            block.move_down()
+            
+
+
+main()
