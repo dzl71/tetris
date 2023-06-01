3d9931e9e42cfc339fe58b0986e8594d5ba5dd67
dzl71 <129626835+dzl71@users.noreply.github.com>
2023-06-02 00:25:57 +0300

    polished the previous change

--- blocks.py
+++ blocks.py
@@ -1,7 +1,10 @@
 import random as rnd
+import os
 
-BLOCK_CHAR = '[]'
-BLANK = "  "
+BOARD_WIDTH = 10
+BOARD_HEIGHT = 20
+BLOCK_CHAR = '1'
+BLANK = "0"
 
 shapes = {
     'I' : 3 * (BLOCK_CHAR + '\n') + BLOCK_CHAR,
@@ -68,14 +71,42 @@ shapes_180 =  {
 }
 
 class block:
-    def __init__(self) -> None:
+    def __init__(self, starting_ptr: tuple[int, int]) -> None:
         self.falling = True
         self.rotated_right_amount = 0
         self.rotated_left_amount = 0
         self.name = rnd.choice(list(shapes))
         self.shape = shapes[self.name]
+        self.row_position_ptr: int = starting_ptr[0]
+        self.column_position_ptr: int = starting_ptr[1]
+    
+    def insert_into_board(self, board: list[list]):
+        row_num = self.row_position_ptr
+        col_num = self.column_position_ptr
+        for blk in self.shape:
+            if blk == '\n':
+                row_num += 1
+                col_num = self.column_position_ptr
+            else:
+                if blk == BLOCK_CHAR:
+                    board[row_num][col_num] = BLOCK_CHAR
+                col_num += 1
+                 
+            
+    def remove_from_board(self, board: list[list]):
+        os.system('clear')
+        row_num = self.row_position_ptr
+        col_num = self.column_position_ptr
+        for blk in self.shape:
+            if blk == '\n':
+                row_num += 1
+                col_num = self.column_position_ptr
+            else:
+                board[row_num][col_num] = BLANK
+                col_num += 1
         
-    def rotate_right(self):
+    def rotate_right(self, board: list[list]):
+        self.remove_from_board(board)
         match self.rotated_right_amount:
             case 0:
                 self.shape = shapes_r[self.name]
@@ -89,19 +120,50 @@ class block:
             case 3:
                 self.shape = shapes[self.name],
                 self.rotated_right_amount = 0
+        self.insert_into_board(board)
     
-    def rotate_left(self):
-        pass
+    def rotate_left(self, board: list[list]):
+        self.remove_from_board(board)
+        match self.rotated_left_amount:
+            case 0:
+                self.shape = shapes_l[self.name]
+                self.rotated_left_amount = 1,
+            case 1: 
+                self.shape = shapes_180[self.name],
+                self.rotated_left_amount = 2
+            case 2:
+                self.shape = shapes_r[self.name],
+                self.rotated_left_amount = 3,
+            case 3:
+                self.shape = shapes[self.name],
+                self.rotated_left_amount = 0
+        self.insert_into_board(board)
     
-    def move_down(self):
-        pass
+    def move_down(self, board: list[list]):
+        self.remove_from_board(board)
+        self.row_position_ptr += 1
+        try:
+            self.insert_into_board(board)
+        except IndexError:
+            self.row_position_ptr -= 1
+        self.insert_into_board(board)
     
-    def fall(self):
-        pass
-    
-    def move_left(self):
-        pass
+    def move_left(self, board: list[list]):
+        self.remove_from_board(board)
+        self.column_position_ptr -= 1
+        try:
+            self.insert_into_board(board)
+        except IndexError:
+            self.column_position_ptr += 1
+        self.insert_into_board(board)
 
-    def move_right(self):
-        pass
+    def move_right(self, board: list[list]):
+        self.remove_from_board(board)
+        self.column_position_ptr += 1
+        try:
+            self.insert_into_board(board)
+        except IndexError:
+            self.column_position_ptr -= 1
+        self.insert_into_board(board)
+        
 
--- main.py
+++ main.py
@@ -4,32 +4,35 @@ import keyboard as kb  # it resolves the problem of the input
 
 BOARD_WIDTH = 10
 BOARD_HEIGHT = 20
-STARTING_POSITION_PTR = 4
+STARTING_POSITION_PTR = tuple([0, 4])
+BLOCK = '[]'
+EMPTY = "  "
 
 def create_board() -> list[list]:
     board = []
     for _ in range(BOARD_HEIGHT):
         row = []
-        for tile in range(BOARD_HEIGHT):
-            row.append(None)
+        for tile in range(BOARD_WIDTH):
+            row.append(blk.BLANK)
         board.append(row)
     return board
 
 def print_board(board: list[list]):
+    print((BOARD_WIDTH  + 1 ) * '--')
     for row in board:
+        print('|', end='')
         for tile in row:
-            if tile == None:
-                print("  ", end='')
+            if tile == blk.BLANK:
+                print(EMPTY, end='')
             else:
-                print(tile, end='')
-        print()
-
-def insert_block_to_board(board: list[list], block: blk.block, position_ptr: int):
-    pass
+                print(BLOCK, end='')
+        print('|')
+    print((BOARD_WIDTH  + 1 ) * '--')
+    time.sleep(0.01)
 
 def is_row_full(board: list[list], row: int) -> bool:
     for tile in board[row]:
-        if tile == None:
+        if tile == 0:
             return False
     return True
 
@@ -40,40 +43,49 @@ def delete_row(board: list[list], row_num: int):
 def board_full(board: list[list]) -> bool:
     return False
 
+def drop_block(board: list[list], block: blk.block):
+    pass
+
 def main():
     board = create_board()
     while not board_full(board):
-        block = blk.block()
-        position_ptr = STARTING_POSITION_PTR
-        insert_block_to_board(board, block, position_ptr)
+        block = blk.block(STARTING_POSITION_PTR)
+        block.insert_into_board(board)
+        print_board(board)
         
         while block.falling:
             
             #insert the checker if able to fall and change the block state
             
-            if kb.is_pressed('d'):
-                block.move_right()
-                insert_block_to_board(board, block, position_ptr)
+            start_time = time.perf_counter()
+            
+            while True:
+                if kb.is_pressed('d'):
+                    block.move_right(board)
+                    print_board(board)
+                    
+                if kb.is_pressed('a'):
+                    block.move_left(board)
+                    print_board(board)
+                    
+                if kb.is_pressed('s'):
+                    drop_block(board, block)
+                    print_board(board)
                 
-            if kb.is_pressed('a'):
-                block.move_left()
-                insert_block_to_board(board, block, position_ptr)
+                if kb.is_pressed('k'):
+                    block.rotate_left(board)
+                    print_board(board)
                 
-            if kb.is_pressed('s'):
-                block.fall()
-                insert_block_to_board(board, block, position_ptr)
-               
-            if kb.is_pressed('k'):
-                block.rotate_left()
-                insert_block_to_board(board, block, position_ptr)
-               
-            if kb.is_pressed('l'):
-                block.rotate_right()
-                insert_block_to_board(board, block, position_ptr)
+                if kb.is_pressed('l'):
+                    block.rotate_right(board)
+                    print_board(board)
                 
-            time.sleep(1)
-            block.move_down()
-            position_ptr += 1
+                end_time = time.perf_counter()
+                if end_time - start_time >= 1:
+                    break
+                
+            block.move_down(board)
+            print_board(board)
             
 
 
