35f2bb6fccf51714cca3fbc159f4456261ce80ff
dzl71 <129626835+dzl71@users.noreply.github.com>
2023-06-06 08:07:31 +0300

    added documentation

--- blocks.py
+++ blocks.py
@@ -16,6 +16,17 @@ STANDART_INPUT_DELAY = 0.0875
 NO_DELAY = 0
 
 def print_board(board: list[list], delay):
+    """
+    The function prints a game board represented by a list of lists with a specified delay between each
+    print.
+    
+    :param board: A 2D list representing the game board, where each element is a tuple containing
+    information about a tile on the board
+    :type board: list[list]
+    :param delay: The delay parameter is a number that represents the amount of time (in seconds) that
+    the program should pause before printing the board. This can be useful for creating animations or
+    for slowing down the output so that it is easier to follow
+    """
     print((BOARD_WIDTH  + 1 ) * '--')
     for row in board:
         print('|', end='')
@@ -28,6 +39,11 @@ def print_board(board: list[list], delay):
     print((BOARD_WIDTH  + 1 ) * '--')
     time.sleep(delay)
 
+# The `shapes` dictionary contains the shapes of the different types of blocks that can appear in the
+# game. Each key in the dictionary represents a different block shape, and the value associated with
+# each key is a tuple containing two elements: the first element is a string representing the shape of
+# the block, and the second element is a tuple containing the dimensions of the block (width and
+# height).
 shapes = {
     'I' : (3 * (BLOCK_CHAR + '\n') + BLOCK_CHAR, (1, 4)),
     
@@ -94,6 +110,18 @@ shapes_180 =  {
 
 class block:
     def __init__(self, starting_ptr: tuple[int, int], serial_num: int) -> None:
+        # This is the constructor method for the `block` class. It initializes the attributes of a
+        # block object, including its direction, name, shape, serial number, falling status, row and
+        # column position pointers, height, and width. The `self.direction` attribute is set to `'up'`
+        # by default, indicating that the block is initially facing upwards. The `self.name` attribute
+        # is randomly chosen from the keys of the `shapes` dictionary, which contains the different
+        # block shapes. The `self.shape` attribute is set to the shape of the block corresponding to
+        # its name. The `self.serial_num` attribute is set to the serial number of the block, which is
+        # used to distinguish it from other blocks on the board. The `self.falling` attribute is set
+        # to `True` by default, indicating that the block is initially falling. The
+        # `self.row_position_ptr` and `self.column_position_ptr` attributes are set to the starting
+        # row and column positions of the block, respectively. The `self.height` and `self.width`
+        # attributes are set to the dimensions of the block's shape.
         self.direction: str = 'up'
         
         self.name = rnd.choice(list(shapes))
@@ -108,11 +136,29 @@ class block:
         self.width = shapes[self.name][1][0]
         
     def change_shape(self, new_shape: dict[str, tuple[str, tuple[int, int]]]):
+        """
+        This function changes the shape, width, and height of an object based on a new shape dictionary.
+        
+        :param new_shape: The parameter `new_shape` is a dictionary that maps a string key to a tuple
+        value. The string key represents the name of a shape, and the tuple value contains two elements:
+        a string representing the shape itself, and a tuple of two integers representing the width and
+        height of the shape. The
+        :type new_shape: dict[str, tuple[str, tuple[int, int]]]
+        """
         self.shape = new_shape[self.name][0]
         self.width = new_shape[self.name][1][0]
         self.height = new_shape[self.name][1][1]
     
     def insert_into_board(self, board: list[list], delay):
+        """
+        This function inserts a shape into a board and prints the updated board with a delay.
+        
+        :param board: a 2D list representing the game board where each element is a tuple containing a
+        character (either a block or empty space) and a serial number (unique identifier for each block)
+        :type board: list[list]
+        :param delay: The delay parameter is a variable that determines the time delay between each step
+        of the game. It is used to control the speed of the game and make it more playable for the user
+        """
         row_num = self.row_position_ptr
         col_num = self.column_position_ptr
         for blk in self.shape:
@@ -127,6 +173,13 @@ class block:
                  
             
     def remove_from_board(self, board: list[list]):
+        """
+        This function removes a shape from a game board by setting the corresponding cells to blank.
+        
+        :param board: a 2D list representing the game board, where each element is a tuple containing a
+        character representing the block type and an integer representing the block's color
+        :type board: list[list]
+        """
         os.system(CLEAR_TERMINAL)
         row_num = self.row_position_ptr
         col_num = self.column_position_ptr
@@ -140,6 +193,13 @@ class block:
                 col_num += 1
         
     def rotate_right(self, board: list[list]):
+        """
+        This function rotates a shape to the right on a game board.
+        
+        :param board: a 2D list representing the game board where the tetromino will be placed and moved
+        on
+        :type board: list[list]
+        """
         self.remove_from_board(board)
         match self.direction:
             case 'up':
@@ -157,6 +217,12 @@ class block:
         self.insert_into_board(board, STANDART_INPUT_DELAY)
     
     def rotate_left(self, board: list[list]):
+        """
+        This function rotates a Tetris piece to the left and updates its position on the game board.
+        
+        :param board: A 2D list representing the game board where the tetromino will be placed
+        :type board: list[list]
+        """
         self.remove_from_board(board)
         match self.direction:
             case 'up':
@@ -174,6 +240,14 @@ class block:
         self.insert_into_board(board, STANDART_INPUT_DELAY)
     
     def move_down(self, board: list[list]):
+        """
+        This function moves a block down on a game board if it is able to fall.
+        
+        :param board: A 2D list representing the game board where the tetromino is currently placed.
+        Each element in the list represents a cell on the board and can have a value of 0 or 1, where 0
+        represents an empty cell and 1 represents a filled cell
+        :type board: list[list]
+        """
         self.remove_from_board(board)
         if self.row_position_ptr + self.height < BOARD_HEIGHT:
             self.falling = self.is_able_to_fall(board)
@@ -182,6 +256,14 @@ class block:
         self.insert_into_board(board, NO_DELAY)
     
     def move_left(self, board: list[list]):
+        """
+        This function moves an object to the left on a board if it is able to, and updates the board
+        accordingly.
+        
+        :param board: A 2D list representing the game board, where each element is a tuple containing
+        information about a block on the board (e.g. its color and serial number)
+        :type board: list[list]
+        """
         able_to_go_left: bool = True
         self.remove_from_board(board)
         if self.column_position_ptr > 0:
@@ -195,6 +277,15 @@ class block:
         self.insert_into_board(board, STANDART_INPUT_DELAY)
 
     def move_right(self, board: list[list]):
+        """
+        This function moves a block to the right on a board if it is able to move without colliding with
+        other blocks.
+        
+        :param board: A 2D list representing the game board, where each element is a tuple containing
+        information about the cell (e.g. whether it is occupied by a block and which block it belongs
+        to)
+        :type board: list[list]
+        """
         able_to_move = True
         self.remove_from_board(board)
         if self.width + self.column_position_ptr < BOARD_WIDTH:
@@ -208,6 +299,17 @@ class block:
         self.insert_into_board(board, STANDART_INPUT_DELAY)
     
     def is_able_to_fall(self, board: list[list]) -> bool:
+        """
+        This function checks if a block can fall down on a given board without colliding with any other
+        blocks.
+        
+        :param board: A 2D list representing the game board, where each element is a tuple containing
+        information about the block at that position. The first element of the tuple represents the type
+        of block (e.g. "I", "J", "L", etc.), and the second element represents the height of the
+        :type board: list[list]
+        :return: The function `is_able_to_fall` returns a boolean value indicating whether or not the
+        current piece can fall further down the board without colliding with any other pieces.
+        """
         if self.row_position_ptr + self.height + 1 > BOARD_HEIGHT:
             return False
         for row_idx in range(self.row_position_ptr, self.row_position_ptr + self.height):
--- main.py
+++ main.py
@@ -10,6 +10,14 @@ EMPTY = "  "
 STANDART_DELAY = 1
 
 def create_board() -> list[list]:
+    """
+    The function creates a two-dimensional list representing a game board with blank tiles and zero
+    values.
+    :return: The function `create_board()` returns a 2D list (list of lists) representing the game
+    board. Each element of the list is a tuple containing a string representing the color of the block
+    and an integer representing the block's rotation. The size of the board is determined by the
+    constants `BOARD_HEIGHT` and `BOARD_WIDTH`.
+    """
     board = []
     for _ in range(BOARD_HEIGHT):
         row = []
@@ -20,33 +28,103 @@ def create_board() -> list[list]:
 
 
 def row_is_full(board: list[list], row: int) -> bool:
+    """
+    The function checks if a given row in a board is completely filled with non-zero tiles.
+    
+    :param board: A 2D list representing the game board, where each element is a tuple representing a
+    tile on the board. The first element of the tuple represents the color of the tile (an integer), and
+    the second element represents whether the tile is empty (0) or occupied (1)
+    :type board: list[list]
+    :param row: The row parameter is an integer representing the index of the row in the board list that
+    we want to check if it is full or not
+    :type row: int
+    :return: a boolean value indicating whether the specified row in the given board is full or not. If
+    all the tiles in the row have a non-zero value in their second element, the function returns True,
+    otherwise it returns False.
+    """
     for tile in board[row]:
         if tile[1] == 0:
             return False
     return True
 
 def delete_row(board: list[list], row_num: int):
+    """
+    The function deletes a row from a board and then drops the remaining rows down.
+    
+    :param board: A 2D list representing the game board, where each element is a tuple containing a
+    block type and its color
+    :type board: list[list]
+    :param row_num: The row number of the board that needs to be deleted
+    :type row_num: int
+    """
     for tile_idx in range(BOARD_WIDTH):
         board[row_num][tile_idx] = (blk.BLANK, 0)
     drop_rows(board, row_num)
 
 def drop_rows(board: list[list], drop_to_row: int):
+    """
+    The function drops rows in a board by shifting the rows above the specified row downwards.
+    
+    :param board: The board parameter is a 2-dimensional list that represents the game board. Each
+    element in the list represents a row on the board, and each row is itself a list of elements
+    representing the tiles in that row
+    :type board: list[list]
+    :param drop_to_row: The parameter `drop_to_row` is an integer that represents the row number to
+    which the rows of the board will be dropped. The function will move all the rows above this row down
+    by one position and replace the contents of the row at `drop_to_row` with the contents of the row
+    above
+    :type drop_to_row: int
+    """
     while drop_to_row > 0:
         for tile_idx in range(BOARD_WIDTH):
             board[drop_to_row][tile_idx] = board[drop_to_row - 1][tile_idx]
         drop_to_row -= 1
 
 def board_full(board: list[list], current_block_serial_num: int) -> bool:
+    """
+    The function checks if the board is full by iterating through the first row of the board and
+    checking if any tile has a non-zero value in the second index and a value less than the current
+    block serial number.
+    
+    :param board: A 2D list representing the game board. Each element in the list is a tuple
+    representing a tile on the board. The first element of the tuple is the value of the tile (0 if it
+    is empty, otherwise the serial number of the block occupying the tile), and the second element is
+    :type board: list[list]
+    :param current_block_serial_num: The current_block_serial_num parameter is an integer representing
+    the serial number of the current block being played on the board
+    :type current_block_serial_num: int
+    :return: a boolean value indicating whether the board is full or not. If there is any tile in the
+    first row of the board that has a non-zero value in the second index and is less than the current
+    block serial number, the function returns True, indicating that the board is full. Otherwise, it
+    returns False, indicating that the board is not full.
+    """
     for tile in board[0]:
         if 0 < tile[1] < current_block_serial_num:
             return True
     return False
 
 def drop_block(board: list[list], block: blk.block):
+    """
+    The function drops a block down on a board until it can no longer fall.
+    
+    :param board: A 2D list representing the game board where the block will be dropped. Each element in
+    the list represents a cell on the board and can contain a value indicating the state of the cell
+    (e.g. empty, filled with a block, etc.)
+    :type board: list[list]
+    :param block: The "block" parameter is an instance of a custom-defined class called "block". This
+    class likely represents a block or shape in a game or puzzle, and has methods for moving and
+    checking its position on a game board. The "drop_block" function takes in a game board represented
+    as a
+    :type block: blk.block
+    """
     while block.is_able_to_fall(board):
         block.move_down(board)
 
 def main():
+    """
+    This function runs a game loop where it creates a board, generates blocks, and allows the player to
+    move and rotate the blocks until they reach the bottom or fill up the board.
+    """
     board = create_board()
     block_num = 1
     while True:
